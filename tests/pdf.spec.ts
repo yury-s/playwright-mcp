abdc5cc8eda5088c1ef7abc67f7d28c01d8cf7cb
Yury Semikhatsky <yurys@chromium.org>
2025-04-14 19:14:14 -0700

    skip pdf on non-chromium

--- tests/pdf.spec.ts
+++ tests/pdf.spec.ts
@@ -30,7 +30,8 @@ test('save as pdf unavailable', async ({ startClient }) => {
   })).toHaveTextContent(/Tool \"browser_pdf_save\" not found/);
 });
 
-test('save as pdf', async ({ client }) => {
+test('save as pdf', async ({ client, browserName }) => {
+  test.skip(browserName !== 'chromium', 'Save as PDF is only supported in Chromium.');
   expect(await client.callTool({
     name: 'browser_navigate',
     arguments: {
