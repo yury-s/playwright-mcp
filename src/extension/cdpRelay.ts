2e5007e12dbf5c399d500f5dcd636520368e2d6f
Yury Semikhatsky <yurys@chromium.org>
2025-08-04 11:03:12 -0700

    chore(extension): build into dist directory

--- src/extension/cdpRelay.ts
+++ src/extension/cdpRelay.ts
@@ -106,7 +106,7 @@ export class CDPRelayServer {
   private _connectBrowser(clientInfo: ClientInfo) {
     const mcpRelayEndpoint = `${this._wsHost}${this._extensionPath}`;
     // Need to specify "key" in the manifest.json to make the id stable when loading from file.
-    const url = new URL('chrome-extension://jakfalbnbhgkpmoaakfflhflbfpkailf/lib/ui/connect.html');
+    const url = new URL('chrome-extension://jakfalbnbhgkpmoaakfflhflbfpkailf/connect.html');
     url.searchParams.set('mcpRelayUrl', mcpRelayEndpoint);
     url.searchParams.set('client', JSON.stringify(clientInfo));
     const href = url.toString();
