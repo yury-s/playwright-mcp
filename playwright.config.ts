db9cfe17205231c41f9b6a951aaa7a655907a077
Pavel Feldman <pavel.feldman@gmail.com>
2025-08-11 12:48:54 -0700

    chore: bump test workers to 2 on CI (#863)

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,8 +22,7 @@ export default defineConfig<TestOptions>({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   reporter: 'list',
   projects: [
     { name: 'chrome' },
