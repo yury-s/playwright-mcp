4370f2cdf2a7492e145fdfb306852af08146c239
Pavel Feldman <pavel.feldman@gmail.com>
2025-08-15 10:19:52 -0700

    chore: try macos15 runners (#892)

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,6 @@ export default defineConfig<TestOptions>({
   reporter: 'list',
   projects: [
     { name: 'chrome' },
-    { name: 'msedge', use: { mcpBrowser: 'msedge' } },
     { name: 'chromium', use: { mcpBrowser: 'chromium' } },
     ...process.env.MCP_IN_DOCKER ? [{
       name: 'chromium-docker',
@@ -38,5 +37,6 @@ export default defineConfig<TestOptions>({
     }] : [],
     { name: 'firefox', use: { mcpBrowser: 'firefox' } },
     { name: 'webkit', use: { mcpBrowser: 'webkit' } },
+    ... process.platform === 'win32' ? [{ name: 'msedge', use: { mcpBrowser: 'msedge' } }] : [],
   ],
 });
